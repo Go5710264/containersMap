806d63c05593309c1ede6ef14744257758f64aa5
Dima Shirkov <dimashirkov@list.ru>
2022-12-02 22:02:59 +0300

    test linting

--- src/__test__/test.js
+++ src/__test__/test.js
@@ -3,13 +3,13 @@ import ErrorsRepository from '../ErrorRepository';
 const errorThrow = new ErrorsRepository();
 
 test.each([
-    [404, 'Ошибка по вине пользователя'],
-    [401, 'Необходима авторизация'],
-    [503, 'Ошибка по вине сервера']
+  [404, 'Ошибка по вине пользователя'],
+  [401, 'Необходима авторизация'],
+  [503, 'Ошибка по вине сервера'],
 ])('testing the release of a bug under the number %d', (errorNumber, errorMassage) => {
-    expect(errorThrow.translate(errorNumber)).toBe(errorMassage)
+  expect(errorThrow.translate(errorNumber)).toBe(errorMassage);
 });
 
 test('throwing an unknown error', () => {
-    expect(errorThrow.translate(400)).toBe('Unknown error');
-})
+  expect(errorThrow.translate(400)).toBe('Unknown error');
+});
